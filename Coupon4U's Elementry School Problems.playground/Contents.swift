634bb3b561e2f7d95e7d34f3f33de13b96afaf25
12341231 <412341>
2017-01-23 16:12:40 +0900

    Coupon4U Problems

---
+++ Coupon4U's Elementry School Problems.playground/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Problem1
+//If list is all true, then isAllTrue is true, otherwise isAllTrue is false;
+let list1:[Bool] = [true, true, true, true, true];
+var isAllTrue:Bool = true;
+
+for bool in list1 {
+    if bool == false {
+        isAllTrue = false;
+    }
+}
+print(isAllTrue, "\n")
+
+
+//Problem2
+//Reverse list
+let list2:[Int] = [1,2,3,4,5,6,7];
+var answer2:[Int] = []
+
+for value in list2 {
+    answer2 = [value] + answer2;
+}
+print(answer2, "\n")
+
+
+//Problem3
+//Print GuGuDan of given array
+let list3:[Int] = [2,4];
+
+for value in list3 {
+    for i in 2...9 {
+        print("\(value) x \(i) = \(value * i)")
+    }
+    print("")
+}
+
+
+//Problem4
+//주어진 배열에서 2의 배수만 뽑아서 새로운 배열을 만든다
+let list4:[Int] = [14,74,25,78,13,79]
+var answer4:[Int] = []
+
+for value in list4 {
+    if value % 2 == 0 {
+        answer4.append(value)
+    }
+}
+print(answer4, "\n")
+
+
+//Problem5
+//주어진 배열의 총합 구하기
+let list5:[Int] = [1,6,3,8,4,0]
+var answer5:Int = 0
+
+for value in list5 {
+    answer5 += value
+}
+print(answer5, "\n")
